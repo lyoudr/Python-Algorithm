9e47c2324734f0712c72efab55b564b780249ba2
Ann <lyoudr@gmail.com>
2020-07-15 12:16:04 +0800

    add READEME.md

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# I use Python to write datastructures and algorithms.
+### I use Python to manipulate various datastructures and algorithms.
+
+# Stack
+Use LIFO to deal with stack
+
+# Queue
+(1) Use FIFO to deal with Queue
+(2) Priority Queue => insert item according to their priority.
+
+# Linked List
+(1) Singly linked list 
+(2) Doubly linkded list 
+
+# Set 
+
+# HashTable
+(1) Dictionary
+(2) HashTable 
+  a. hash function => loseloseHashCode
+
+# Tree
+(1) Binary search tree
+(2) Traverse 
+  a. in ordert traverse
+  b. pre order traverse
+  c. post order traverse
+
+# Sort 
+(1) Bubble Sort
+(2) Selection Sort
+(3) Insertion Sort
+(4) Merge Sort
+(5) Quick Sort
+
+# Search
+(1) Sequential Search
+(2) Binary Search
--- queue.py
+++ queue.py
@@ -1,3 +1,5 @@
+# Queue 
+# apply FIFO
 class Queue:
   def __init__(self):
     self.items = []
@@ -11,3 +13,42 @@ class Queue:
     return self.items[0]
   def clear(self):
     self.items.clear()
+
+# Priority Queue
+class QueueElement:
+  def __init__(self, element, priority):
+    self.element = element
+    self.priority = priority
+
+class PriorityQueue:
+  def __init__(self):
+    self.items = []
+  def enqueue(self, element, priority):
+    queue_ele = QueueElement(element, priority)
+    if self.isEmpty():
+      self.items.append(queue_ele)
+    else:
+      added = False
+      for index, item in enumerate(self.items):
+        if priority <= item.priority :
+          self.items.insert(index, queue_ele)
+          break
+      if not added:
+        self.items.append(queue_ele)
+  def dequeue(self):
+    return self.items.pop(0)
+  def isEmpty(self):
+    return len(self.items) == 0
+  def front(self):
+    return self.items[0]
+  def clear(self):
+    self.items.clear()
+  def print_out(self):
+    print('items is =>',[(item.element, item.priority) for item in self.items])
+
+p_queue = PriorityQueue()
+p_queue.enqueue('Ann', 2)
+p_queue.enqueue('John', 1)
+p_queue.enqueue('Judy', 4)
+p_queue.enqueue('Mark', 3)
+p_queue.print_out()
